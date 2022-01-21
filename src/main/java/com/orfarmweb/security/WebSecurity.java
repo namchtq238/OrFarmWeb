890c07626d38d16fcdb59f8bff534ecd2c991c65
Thịnh Trần Nguyên <84261217+thinh1407@users.noreply.github.com>
2022-01-21 10:26:15 +0700

    Update WebSecurity.java

--- src/main/java/com/orfarmweb/security/WebSecurity.java
+++ src/main/java/com/orfarmweb/security/WebSecurity.java
@@ -44,6 +44,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
         http.authorizeRequests()
                 .antMatchers("/resources/**").permitAll()
                 .antMatchers("/admin/**").hasAuthority(Role.ADMIN.getType())
