ac4b15fb5f76008cd9af04a8fb044ceb35e98d52
soan27 <hoangthixoan272000@gmail.com>
2022-01-23 20:48:37 +0700

    ViewCart

--- src/main/resources/static/assets/js/main.js
+++ src/main/resources/static/assets/js/main.js
@@ -520,31 +520,18 @@
 
         //===== product quantity
 
-        $('.add').click(function () {
+        $('#add').click(function () {
             if ($(this).prev().val()) {
                 $(this).prev().val(+$(this).prev().val() + 1);
             }
         });
-        $('.sub').click(function () {
+        $('#sub').click(function () {
             if ($(this).next().val() > 1) {
                 if ($(this).next().val() > 1) $(this).next().val(+$(this).next().val() - 1);
             }
         });
 
-
-
-
-
-
-
-
-
-
-
     });
-
-
-
 
 })(jQuery);
 //input plus sub 
