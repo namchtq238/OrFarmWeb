bfe8935cb5f568985629e924a4c0abc9c38d8cef
soan27 <hoangthixoan272000@gmail.com>
2022-01-24 23:15:00 +0700

    Add view Admin

--- src/main/java/com/orfarmweb/controller/AdminController.java
+++ src/main/java/com/orfarmweb/controller/AdminController.java
@@ -10,4 +10,19 @@ public class AdminController {
 
     @RequestMapping("/orderAdmin")
     public String orderAmin(){return "admin-page/order";}
+
+    @RequestMapping("/productAdmin")
+    public String productAdmin(){return "admin-page/product";}
+
+    @RequestMapping("/addProductAdmin")
+    public String addProductAdmin(){return "admin-page/add-product";}
+
+    @RequestMapping("/hubAdmin")
+    public String hubAdmin(){return "admin-page/hub";}
+
+    @RequestMapping("/staffAdmin")
+    public String staff(){return "admin-page/staff";}
+
+    @RequestMapping("/userManagerAdmin")
+    public String userManagerAdmin(){return "/admin-page/user";}
 }
