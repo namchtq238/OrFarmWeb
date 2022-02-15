dc5e66d084a04401b63cc3c8ce608fb891fa5e44
Thịnh Trần Nguyên <84261217+thinh1407@users.noreply.github.com>
2022-02-15 20:26:53 +0700

    xong statistic

--- src/main/java/com/orfarmweb/repository/OrderDetailRepo.java
+++ src/main/java/com/orfarmweb/repository/OrderDetailRepo.java
@@ -35,6 +35,6 @@ public interface OrderDetailRepo extends JpaRepository<OrderDetail, Integer> {
             "from order_detail left join orders on order_detail.order_id = orders.id left join product on order_detail.product_id = product.id " +
             "where not (orders.create_at >= :end or orders.create_at <= :start) group by order_detail.product_id", nativeQuery = true)
     List<OrderDetail> findOrderDetailByDay(Date start, Date end);
-    @Query(value = "select count(order_detail.product_id) from order_detail left join orders on order_detail.order_id = orders.id left join product on order_detail.product_id = product.id where not (orders.create_at >= :end or orders.create_at <= :start) and order_detail.product_id = :id", nativeQuery = true)
+    @Query(value = "select sum(order_detail.quantity) from order_detail left join orders on order_detail.order_id = orders.id left join product on order_detail.product_id = product.id where not (orders.create_at >= :end or orders.create_at <= :start) and order_detail.product_id = :id", nativeQuery = true)
     Integer getTotalProductByDay(Date start, Date end, int id);
 }
